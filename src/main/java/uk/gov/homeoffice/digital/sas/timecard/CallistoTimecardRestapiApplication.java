e4792080e18874bba6539c4168ddf74bc793afb3
Velmurugan <velmurugan.muthu@digital.homeoffice.gov.uk>
2022-06-09 12:34:37 +0100

    basic project structure

---
+++ src/main/java/uk/gov/homeoffice/digital/sas/timecard/CallistoTimecardRestapiApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.homeoffice.digital.sas.timecard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallistoTimecardRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallistoTimecardRestapiApplication.class, args);
+	}
+
+}
