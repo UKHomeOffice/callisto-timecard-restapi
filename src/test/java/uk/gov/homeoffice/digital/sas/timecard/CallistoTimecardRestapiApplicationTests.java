b3c710511c8691c02ed972da0ab73298806c2021
daleelR <daleel.ramjan1@digital.homeoffice.gov.uk>
2022-08-12 15:31:58 +0100

    Add JpaRest dependency. Server config currently only in local settings.xml

--- src/test/java/uk/gov/homeoffice/digital/sas/timecard/CallistoTimecardRestapiApplicationTests.java
+++ src/test/java/uk/gov/homeoffice/digital/sas/timecard/CallistoTimecardRestapiApplicationTests.java
@@ -1,9 +1,10 @@
 package uk.gov.homeoffice.digital.sas.timecard;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class CallistoTimecardRestapiApplicationTests {
 
 	@Test
