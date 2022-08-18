b8516b6ecc0d21525b4485b48ae92f5fd3724496
joannafawl <joanna.fawl@madetech.com>
2022-08-18 11:39:23 +0100

    Tidy up debugging code

--- src/main/java/uk/gov/homeoffice/digital/sas/timecard/CallistoTimecardRestapiApplication.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/timecard/CallistoTimecardRestapiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CallistoTimecardRestapiApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Helloooo");
 		SpringApplication.run(CallistoTimecardRestapiApplication.class, args);
 	}
 
