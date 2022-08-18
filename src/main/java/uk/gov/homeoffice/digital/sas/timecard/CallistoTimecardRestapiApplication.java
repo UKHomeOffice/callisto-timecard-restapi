4876d69240d6d4e537b44fbd84f755c48a43f1c1
joannafawl <joanna.fawl@madetech.com>
2022-08-18 11:09:53 +0100

    Add build image step back in for restapi

--- src/main/java/uk/gov/homeoffice/digital/sas/timecard/CallistoTimecardRestapiApplication.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/timecard/CallistoTimecardRestapiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CallistoTimecardRestapiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Helloooo");
 		SpringApplication.run(CallistoTimecardRestapiApplication.class, args);
 	}
 
