39d26727ba57bd15c6029c07f8a1f59049965e86
joannafawl <joanna.fawl@madetech.com>
2022-10-17 12:19:11 +0100

    Check time entry id in query to exclude from clashing time entries

--- src/main/java/uk/gov/homeoffice/digital/sas/timecard/validators/timeentry/TimeEntryValidator.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/timecard/validators/timeentry/TimeEntryValidator.java
@@ -1,15 +1,22 @@
 package uk.gov.homeoffice.digital.sas.timecard.validators.timeentry;
 
+import org.hibernate.FlushMode;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.homeoffice.digital.sas.jparest.exceptions.ResourceConstraintViolationException;
 import uk.gov.homeoffice.digital.sas.timecard.model.TimeEntry;
 import uk.gov.homeoffice.digital.sas.timecard.repositories.TimeEntryRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @Service
 public class TimeEntryValidator {
 
     private final TimeEntryRepository timeEntryRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Autowired
     public TimeEntryValidator(TimeEntryRepository timeEntryRepository) {
@@ -18,16 +25,18 @@ public class TimeEntryValidator {
 
 
     public void validate(TimeEntry timeEntry) {
-
+        Session session = entityManager.unwrap(Session.class);
+        session.setHibernateFlushMode(FlushMode.MANUAL);
+        var timeEntryId = timeEntry.getId() != null ? timeEntry.getId().toString() : null;
         if (!timeEntryRepository.findAllClashingTimeEntries(
                 timeEntry.getOwnerId(),
+                timeEntryId,
                 timeEntry.getActualStartTime(),
                 timeEntry.getActualEndTime()).isEmpty()) {
             throw new ResourceConstraintViolationException(
                     "Cannot have clashes");
         }
-
-
+        session.setHibernateFlushMode(FlushMode.AUTO);
     }
 
 }
