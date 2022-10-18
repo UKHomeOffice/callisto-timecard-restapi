67cbabfb536b5bf5efa2fb8be822fa97ee070fc9
Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>
2022-10-18 18:24:52 +0100

    Working version of BDD tests using Cucumber JPA Rest Test library. Service registry has to be injected as a system property: -Dservices=timecard=http://localhost:9090

---
+++ src/test/java/uk/gov/homeoffice/digital/sas/timecard/bdd/TimeCardBddTest.java
@@ -0,0 +1,12 @@
+package uk.gov.homeoffice.digital.sas.timecard.bdd;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"classpath:uk.gov.homeoffice.digital.sas.timecard.bdd"},
+        glue = "uk.gov.homeoffice.digital.sas.cucumberjparest")
+public class TimeCardBddTest {
+}
