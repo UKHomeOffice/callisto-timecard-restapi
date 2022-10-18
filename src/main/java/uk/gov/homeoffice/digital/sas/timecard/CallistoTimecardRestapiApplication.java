d1d3b66946462fcffb73aa991cc69f587528b392
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2022-10-18 15:59:34 +0100

    eahw-2251-add-maven-checkstyle: update files to match google checkstyle

--- src/main/java/uk/gov/homeoffice/digital/sas/timecard/CallistoTimecardRestapiApplication.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/timecard/CallistoTimecardRestapiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CallistoTimecardRestapiApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(CallistoTimecardRestapiApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(CallistoTimecardRestapiApplication.class, args);
+  }
 }
