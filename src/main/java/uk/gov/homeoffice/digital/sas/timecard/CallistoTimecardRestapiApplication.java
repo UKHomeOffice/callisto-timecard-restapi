7ba54caa02275478ff782b1a836607732cb347d8
Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>
2022-10-24 16:51:24 +0100

    Add new dummy code lines to test sonar scan

--- src/main/java/uk/gov/homeoffice/digital/sas/timecard/CallistoTimecardRestapiApplication.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/timecard/CallistoTimecardRestapiApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CallistoTimecardRestapiApplication {
   public static void main(String[] args) {
+    dummyMethod();
     SpringApplication.run(CallistoTimecardRestapiApplication.class, args);
+  }
+
+  private static void dummyMethod() {
+    System.out.println("dummyMethod");
   }
 }
