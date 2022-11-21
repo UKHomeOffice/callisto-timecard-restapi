03c2c8acf457cbc8acc10b7f35cc2d84ff05fa44
Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>
2022-11-21 19:55:23 +0000

    load service registry definition from relevant system property

--- src/test/java/uk/gov/homeoffice/digital/sas/timecard/bdd/TestSetupStepDefinitions.java
+++ src/test/java/uk/gov/homeoffice/digital/sas/timecard/bdd/TestSetupStepDefinitions.java
@@ -3,7 +3,6 @@ package uk.gov.homeoffice.digital.sas.timecard.bdd;
 import static uk.gov.homeoffice.digital.sas.cucumberjparest.persona.PersonaManager.TENANT_ID_SYSTEM_PROPERTY_NAME;
 
 import io.cucumber.java.BeforeAll;
-import java.util.Map;
 import java.util.UUID;
 import uk.gov.homeoffice.digital.sas.cucumberjparest.api.JpaRestApiClient;
 import uk.gov.homeoffice.digital.sas.cucumberjparest.api.ServiceRegistry;
@@ -11,16 +10,14 @@ import uk.gov.homeoffice.digital.sas.cucumberjparest.persona.Persona;
 
 public class TestSetupStepDefinitions {
 
-
   @BeforeAll
   public static void setup() {
     UUID tenantId = UUID.randomUUID();
     System.setProperty(TENANT_ID_SYSTEM_PROPERTY_NAME, tenantId.toString());
 
-    // TODO: get it from the system property
-    Map<String, String> services = Map.of("timecard","http://localhost:9090");
-
-    JpaRestApiClient jpaRestApiClient = new JpaRestApiClient(new ServiceRegistry(services));
+    // Instantiating the API client bean explicitly. Autowiring it from Spring context doesn't work
+    // in cucumber @BeforeAll
+    JpaRestApiClient jpaRestApiClient = new JpaRestApiClient(new ServiceRegistry());
     Persona admin = new Persona();
     admin.setTenantId(tenantId);
 
