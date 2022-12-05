3c0679df5889c42376f4a7b051eb430d5736f0bc
daleelR <daleel.ramjan1@digital.homeoffice.gov.uk>
2022-12-05 11:59:28 +0000

    remove push event restriction from check promotion script.

--- pipeline-scripts/check-build-promotion.sh
+++ pipeline-scripts/check-build-promotion.sh
@@ -1,6 +1,6 @@
 LAST_BUILD=$(
   drone build ls --format '{{.Number}}' --branch "$DRONE_BRANCH" --status success \
-  --event push --limit 100 "$DRONE_REPO" | head -n 1
+  --limit 100 "$DRONE_REPO" | head -n 1
 )
 if [ $LAST_BUILD != $DRONE_BUILD_PARENT ]; then
   echo 'Could not promote as last build: ' $LAST_BUILD ' was not successful';
