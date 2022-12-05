a404b990525b477df8956bb67d64538ff7341304
daleelR <daleel.ramjan1@digital.homeoffice.gov.uk>
2022-12-05 11:18:05 +0000

    Move drone promotion check commands to separate script.

---
+++ pipeline-scripts/check-build-promotion.sh
@@ -0,0 +1,8 @@
+LAST_BUILD=$(
+  drone build ls --format '{{.Number}}' --branch "$DRONE_BRANCH" --status success \
+  --event push --limit 100 "$DRONE_REPO" | head -n 1
+)
+if [ $LAST_BUILD != $DRONE_BUILD_PARENT ]; then
+  echo 'Could not promote as last build: ' $LAST_BUILD ' was not successful';
+  exit 1;
+fi
