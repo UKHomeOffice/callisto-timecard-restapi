25609b076a869b654f8f8f3808bafe8e069ce3e9
daleelR <daleel.ramjan1@digital.homeoffice.gov.uk>
2022-12-06 15:24:36 +0000

    Move sonarqube commands back to yaml file

--- pipeline-scripts/run-sonarqube.sh
+++
@@ -1,1 +0,0 @@
-mvn -s ./timecard_settings.xml sonar:sonar -Dsonar.host.url=$${SONAR_HOST} -Dsonar.login=$${SONAR_TOKEN} -Dsonar.organization=ukhomeoffice -Dsonar.projectKey=callisto-timecard-restapi -Dsonar.branch.name=$DRONE_BRANCH -Dsonar.projectName=callisto-timecard-restapi -Dsonar.qualitygate.wait=true
