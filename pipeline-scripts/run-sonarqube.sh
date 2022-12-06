6041c9336fc1b600178d50f7180fd5806586f656
daleelR <daleel.ramjan1@digital.homeoffice.gov.uk>
2022-12-06 11:54:53 +0000

    remove tabs from script

--- pipeline-scripts/run-sonarqube.sh
+++ pipeline-scripts/run-sonarqube.sh
@@ -1,8 +1,8 @@
-mvn -s ./timecard_settings.xml sonar:sonar
-  -Dsonar.host.url=$${SONAR_HOST}
-  -Dsonar.login=$${SONAR_TOKEN}
-  -Dsonar.organization=ukhomeoffice
-  -Dsonar.projectKey=callisto-timecard-restapi
-  -Dsonar.branch.name=$DRONE_BRANCH
-  -Dsonar.projectName=callisto-timecard-restapi
-  -Dsonar.qualitygate.wait=true
+mvn -s ./timecard_settings.xml sonar:sonar \
+-Dsonar.host.url=$${SONAR_HOST} \
+-Dsonar.login=$${SONAR_TOKEN} \
+-Dsonar.organization=ukhomeoffice \
+-Dsonar.projectKey=callisto-timecard-restapi \
+-Dsonar.branch.name=$DRONE_BRANCH \
+-Dsonar.projectName=callisto-timecard-restapi \
+-Dsonar.qualitygate.wait=true
