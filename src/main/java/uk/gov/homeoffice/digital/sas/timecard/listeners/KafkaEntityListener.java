0a55ebab0793dcaf28164c56f1ce42ae9487fac1
joannafawl <joanna.fawl@madetech.com>
2022-12-14 16:09:05 +0000

    wip move createProducerService into TimeEntryKafkaEventListener

--- src/main/java/uk/gov/homeoffice/digital/sas/timecard/listeners/KafkaEntityListener.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/timecard/listeners/KafkaEntityListener.java
@@ -2,22 +2,14 @@ package uk.gov.homeoffice.digital.sas.timecard.listeners;
 
 import javax.persistence.PostPersist;
 import javax.persistence.PostUpdate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import uk.gov.homeoffice.digital.sas.timecard.enums.KafkaAction;
 import uk.gov.homeoffice.digital.sas.timecard.kafka.producers.KafkaProducerService;
 
-@Component
 public abstract class KafkaEntityListener<T> {
 
-  private KafkaProducerService<T> kafkaProducerService;
+  protected KafkaProducerService<T> kafkaProducerService;
 
   public abstract String resolveMessageKey(T resource);
-
-  @Autowired
-  public void createProducerService(KafkaProducerService<T> kafkaProducerService) {
-    this.kafkaProducerService = kafkaProducerService;
-  }
 
   @PostPersist
   protected void sendKafkaMessageOnCreate(T resource) {
--- src/main/java/uk/gov/homeoffice/digital/sas/timecard/listeners/TimeEntryKafkaEntityListener.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/timecard/listeners/TimeEntryKafkaEntityListener.java
@@ -1,11 +1,20 @@
 package uk.gov.homeoffice.digital.sas.timecard.listeners;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.homeoffice.digital.sas.timecard.kafka.producers.KafkaProducerService;
 import uk.gov.homeoffice.digital.sas.timecard.model.TimeEntry;
 
+@Component
 public class TimeEntryKafkaEntityListener extends KafkaEntityListener<TimeEntry> {
 
   @Override
   public String resolveMessageKey(TimeEntry timeEntry) {
     return timeEntry.getOwnerId().toString();
+  }
+
+  @Autowired
+  public void createProducerService(KafkaProducerService<TimeEntry> kafkaProducerService) {
+    this.kafkaProducerService = kafkaProducerService;
   }
 }
