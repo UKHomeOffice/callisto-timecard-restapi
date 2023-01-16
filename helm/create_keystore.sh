8ab9dbe8ff015834fdb31efff18ac3e4b7921f4d
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2023-01-16 17:39:41 +0000

    Initial script and init container

--- helm/create_keystore.sh
+++ helm/create_keystore.sh
@@ -1,43 +1,96 @@
 #!/usr/bin/env sh
 set -e
 
-dir=/keystore/$1
+dir=keystore
 alias=$1
-days=14600
-password=changeit
+days=100
+password=$2
 
-if test -f "$dir/${alias}_creds"; then
-    echo Keystore already created
-    exit
+aws configure set aws_access_key_id $3
+aws configure set aws_secret_access_key $4
+aws configure set aws_region eu-west-2
+
+ca_arn=$5
+
+aws acm-pca get-certificate --certificate-authority-arn  \
+  --certificate-arn  \
+  | jq '.Certificate, .CertificateChain' | sed 's/\\n/\n/g' | tr -d \" > ${dir}/${alias}_certificate.pem
+
+if openssl x509 -checkend 86400 -noout -in ${dir}/${alias}_certificate.pem
+then
+  echo "Certificate is valid"
+  keytool -keystore ${dir}/${alias}.keystore.jks -alias Callisto -import -noprompt -file ./"${dir}"/"${alias}"_certificate.pem -storepass "${password}" -keypass "${password}"
+  cp /opt/openjdk-17/lib/security/cacerts "${dir}"/"${alias}".truststore.jks
+  exit
 fi
+
+echo "Certificate has expired"
+
+#rm -r ${dir}/${alias}_certificate.pem
+
+echo "Creating new certificate"
 
 rm -rf $dir
 mkdir -p $dir
 
 # Create a private key
-keytool -genkey -validity $days -alias $alias -dname "C=GB, O=UK Home Office, CN=Callisto $alias" -keystore $dir/$alias.keystore.jks -keyalg RSA  -storepass $password  -keypass $password -storetype pkcs12
+if
+  keytool -genkey -validity $days -alias $alias -dname "C=GB, O=UK Home Office, CN=Callisto $alias" -keystore $dir/$alias.keystore.jks -keyalg RSA -storepass $password -keypass $password
+then
+  echo "Created private key"
+else echo "Creating private key failed"
+exit
+fi
 
 # Create CSR
-keytool -keystore $dir/$alias.keystore.jks -alias $alias -certreq -file $dir/$alias.csr -storepass $password -keypass $password
-
-#TODO - will need to find the args to set the details automatically.
-aws configure --profile pca
+if
+  keytool -keystore $dir/$alias.keystore.jks -alias $alias -certreq -file $dir/$alias.csr -storepass $password -keypass $password
+  sed -e 's/\ NEW//g' $dir/$alias.csr > $dir/$alias_temp.csr && mv $dir/$alias_temp.csr $dir/$alias.csr
+then
+  echo "Created CSR"
+else echo "Creating CSR failed"
+exit
+fi
 
 # Create cert signed by CA
-aws acm-pca issue-certificate --certificate-authority-arn $cert_arn --csr fileb://$dir/$alias.csr --signing-algorithm "SHA256WITHRSA" --validity Value=300,Type="DAYS" --profile pca
-# TODO - this command print a arn to the command line. We will need to extract that
+if
+  ARN=$(aws acm-pca issue-certificate --certificate-authority-arn $ca_arn --csr fileb://$dir/$alias.csr --signing-algorithm "SHA256WITHRSA" --validity Value=$days,Type="DAYS" --profile pca --output text)
+then
+  echo "Arn Stored"
+else echo "Arn not stored"
+exit
+fi
+
+aws acm-pca wait certificate-issued --certificate-authority-arn $ca_arn --certificate-arn $ARN
+if [ $? -eq 255 ]
+then
+  echo "Certificate not issue"
+else
+  echo "Certificate issued" >&2
+fi
 
 # Get Certificate from arn
-aws acm-pca get-certificate --certificate-authority-arn $cert_arn --certificate-arn <Insert CertificateArn value from previous step> --profile pca | jq '.Certificate, .CertificateChain' | sed 's/\\n/\n/g' | tr -d \" > certificate.pem
+if
+  echo $ARN
+  aws acm-pca get-certificate --certificate-authority-arn $ca_arn --certificate-arn $ARN | jq '.Certificate, .CertificateChain' | sed 's/\\n/\n/g' | tr -d \" > $dir/$alias.pem
+then
+  echo "Certificate retrieved"
+else echo "Retrieving certificate failed"
+exit
+fi
 
 # Import cert into keystore
-keytool -keystore $dir/$alias.keystore.jks -alias Callisto -import -noprompt -file ./certificate.pem-storepass $password -keypass $password
+if
+  keytool -keystore $dir/$alias.keystore.jks -alias Callisto -import -noprompt -file $dir/$alias.pem -storepass $password -keypass $password
+then
+  echo "stored certificate in keystore"
+else echo "Failed to store certificate in keystore"
+exit
+fi
 
 # cp truststore into desired location
-cp /opt/openjdk-17/lib/security/cacerts kafka.client.truststore.jks
-
-echo -n $password > $dir/${alias}_creds
-
-# Inspect keystore contents
-# keytool -list -v -keystore $dir/$alias.keystore.jks -storepass $password
-# keytool -list -v -keystore $dir/$alias.truststore.jks -storepass $password
+if
+  cp /opt/openjdk-17/lib/security/cacerts $dir/$alias.truststore.jks
+then
+  echo "Truststore copied"
+fi
