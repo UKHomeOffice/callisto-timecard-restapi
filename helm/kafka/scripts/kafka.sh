0e0f5acece43df8aef6328c889112087faf13e8f
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2023-02-03 16:06:25 +0000

    remove comments

--- helm/kafka/scripts/kafka.sh
+++ helm/kafka/scripts/kafka.sh
@@ -4,14 +4,14 @@ set -e
 bootstrap_server=$1
 keystore_password=$2
 
-#cd /tmp
-#cp /scripts/timecard-properties /tmp
+cd /tmp
+cp /scripts/timecard-properties /tmp
 
 echo "Starting kafka.sh"
 #put password in timecard-properties
 sed 's/#/'$keystore_password'/g' timecard-properties > timecard-properties-temp && mv timecard-properties-temp timecard-properties
 
-#cd /scripts
+cd /scripts
 . ./create-topic.sh
 create_topic $bootstrap_server
 apply_permissions
