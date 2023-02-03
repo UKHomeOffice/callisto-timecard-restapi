cd049cb4ab17f82a5826df739d4e56f36740157d
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2023-02-03 11:26:16 +0000

    add a step to move into the correct dir

--- helm/kafka/scripts/kafka.sh
+++ helm/kafka/scripts/kafka.sh
@@ -4,6 +4,9 @@ bootstrap_url=$2
 topic_dir=$3
 properties_file=$topic_dir/timecard-properties
 keystore_password=$1
+cd $topic_dir
+cp /scripts/timecard-properties $topic_dir
+ls -ltr
 
 echo "Starting kafka.sh"
 #put password in timecard-properties
