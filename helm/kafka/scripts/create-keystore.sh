1d9096615abd3a887fe199558c0492cf8d8dc978
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2023-02-03 14:21:43 +0000

    Add mounts back

--- helm/kafka/scripts/create-keystore.sh
+++ helm/kafka/scripts/create-keystore.sh
@@ -18,7 +18,6 @@ then
   echo "Certificate already created, checking validity..."
     if openssl x509 -checkend 86400 -noout -in $service_alias-certificate.pem &&
       openssl s_client -connect $bootstrap_server_url -key $service_alias-key.pem -cert $service_alias-certificate.pem -brief
-      cp -r /$keystore_dir /tmp
     then
       echo "Certificate is valid, exiting"; exit 0;
     fi
