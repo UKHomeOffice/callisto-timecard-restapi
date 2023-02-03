7a708a126d087359fcf4ac2007c630f5b31790f5
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2023-02-03 16:35:32 +0000

    add if statement around sed update bootstrap

--- helm/kafka/scripts/create-topic.sh
+++ helm/kafka/scripts/create-topic.sh
@@ -8,11 +8,22 @@ keystore_password=$2
 topic=callisto-timecard
 # Ensures a topic exists
 
-#put password in timecard-properties
-sed 's/#/'$keystore_password'/g' timecard-properties > timecard-properties-temp && mv timecard-properties-temp timecard-properties
+cp scripts/timecard-properties tmp/
+cd /tmp
 
+#put password in timecard-properties
+if sed 's/#/'$keystore_password'/g' timecard-properties > timecard-properties-temp && mv timecard-properties-temp timecard-properties
+then
+  echo "properties file updated"
+else
+  echo "properties file failed to update"
+  exit 1
+fi
+
+cd /bin/bash
 echo "Checking if topic is available"
-if kafka-topics.sh --bootstrap-server $bootstrap_url--command-config $properties_file \
+echo $bootstrap_server
+if kafka-topics.sh --bootstrap-server $bootstrap_server --command-config $properties_file \
         --create --topic $topic --if-not-exists \
         > /dev/null
 then
