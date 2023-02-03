449222c3610b7016b48ee691ae76f6549b9a00cb
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2023-02-03 16:03:09 +0000

    update shebang on kafka.sh

--- helm/kafka/scripts/kafka.sh
+++ helm/kafka/scripts/kafka.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 bootstrap_server=$1
 keystore_password=$2
 
-cd /tmp
-cp /scripts/timecard-properties /tmp
+#cd /tmp
+#cp /scripts/timecard-properties /tmp
 
 echo "Starting kafka.sh"
 #put password in timecard-properties
 sed 's/#/'$keystore_password'/g' timecard-properties > timecard-properties-temp && mv timecard-properties-temp timecard-properties
 
-cd /scripts
+#cd /scripts
 . ./create-topic.sh
 create_topic $bootstrap_server
 apply_permissions
