38c6bdd339ffdc6b18a63b58c62c3f2e80324f44
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2023-02-03 17:13:36 +0000

    add permissions step

--- helm/kafka/scripts/create-topic.sh
+++ helm/kafka/scripts/create-topic.sh
@@ -28,3 +28,49 @@ if kafka-topics.sh --bootstrap-server $bootstrap_server --command-config $proper
 then
   echo Topic available: $topic
 fi
+
+# read through the contents of the permissions file and
+# create the permissions. Ignore empty lines and comments
+IFS=$'\n' acl_config=$(grep --color=never "^[^#].*" $root_path/permissions.txt)
+unset IFS
+for line in "${acl_config[@]}"
+do
+# skip empty lines
+if [ -z "$line" ]; then continue; fi
+details=$line
+
+# if first argument is --topic assume a new list of permissions are being specified
+if [ "${details[0]}" = "--topic" ]
+then
+# if permissions have already been specified for a previous topic
+# apply them.
+if [ -n "$topic" ]
+then
+  IFS=$'\n'
+  set_permissions $topic $pattern_type "${permissions[*]/%/$'\n'}"
+  unset IFS
+fi
+  # Reset the variables
+  topic=${details[1]}
+  pattern_type=${details[3]}
+  permissions=
+  else
+
+    # Add the desired permisions to the current permission
+    # array for this topic
+    principal=${details[0]}
+    operation=${details[1]}
+    permission=${details[2]}
+
+    permissions+="$principal $operation $permission"
+  fi
+
+done
+    # The end of the file has been reached. If a topic
+    # was set apply the permissions.
+    if [ -n "$topic" ]
+    then
+        IFS=$'\n'
+        set_permissions $topic $pattern_type "${permissions[*]/%/$'\n'}"
+        unset IFS
+    fi
