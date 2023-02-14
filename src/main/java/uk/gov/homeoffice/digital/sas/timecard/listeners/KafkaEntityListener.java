d895f77523d11626d286049773484db72c3fe5c5
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2023-02-14 13:10:29 +0000

    Add transactional synchronisation and log messages

--- src/main/java/uk/gov/homeoffice/digital/sas/timecard/listeners/KafkaEntityListener.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/timecard/listeners/KafkaEntityListener.java
@@ -1,8 +1,12 @@
 package uk.gov.homeoffice.digital.sas.timecard.listeners;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 import uk.gov.homeoffice.digital.sas.timecard.enums.KafkaAction;
 import uk.gov.homeoffice.digital.sas.timecard.kafka.producers.KafkaProducerService;
 
+@Slf4j
 public abstract class KafkaEntityListener<T> {
 
   protected KafkaProducerService<T> kafkaProducerService;
@@ -13,21 +17,51 @@ public abstract class KafkaEntityListener<T> {
     this.kafkaProducerService = kafkaProducerService;
   }
 
-  protected void sendKafkaMessageOnCreate(T resource) {
-    sendMessage(resource, KafkaAction.CREATE);
+  protected void sendKafkaMessageOnCreate(T resource, String topicName) {
+      sendMessage(resource, KafkaAction.CREATE, topicName);
   }
 
-  protected void sendKafkaMessageOnUpdate(T resource) {
-    sendMessage(resource, KafkaAction.UPDATE);
+  protected void sendKafkaMessageOnUpdate(T resource, String topicName) {
+    sendMessage(resource, KafkaAction.UPDATE, topicName);
   }
 
-  protected void sendKafkaMessageOnDelete(T resource) {
-    sendMessage(resource, KafkaAction.DELETE);
+  protected void sendKafkaMessageOnDelete(T resource, String topicName) {
+    sendMessage(resource, KafkaAction.DELETE, topicName);
   }
 
   @SuppressWarnings("unchecked")
-  private void sendMessage(T resource, KafkaAction action) {
-    kafkaProducerService.sendMessage(resolveMessageKey(resource),
-        (Class<T>) resource.getClass(), resource, action);
+  private void sendMessage(T resource, KafkaAction action, String topicName) {
+    TransactionSynchronizationManager.registerSynchronization(
+        new TransactionSynchronization() {
+          int status = TransactionSynchronization.STATUS_UNKNOWN;
+          String messageKey = resolveMessageKey(resource);
+
+          @Override
+          public void beforeCommit(boolean readOnly) {
+            kafkaProducerService.sendMessage(messageKey,
+                (Class<T>) resource.getClass(), resource, action);
+          }
+
+          @Override
+          public void afterCommit(){
+            status = TransactionSynchronization.STATUS_COMMITTED;
+          }
+
+          @Override
+          public void afterCompletion(int status){
+            if (status == STATUS_COMMITTED) {
+              log.info(String.format("Database transaction successful with messageKey [ %s ] sent" +
+                      " to " +
+                  "topic [ %s ], with action [ %s ]", messageKey, topicName,
+                  action.toString()));
+
+            } else {
+              log.error(String.format("Database transaction failed with messageKey [%s] sent to " +
+                  "topic " +
+                  "[%s], with action [%s]", messageKey, topicName, action.toString()));
+            }
+          }
+        }
+    );
   }
 }
