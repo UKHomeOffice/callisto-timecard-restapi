b8c85cdf9d516c28a261f4c4a989e828dd26e2f7
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2023-02-21 15:15:47 +0000

    Remove comments from kafkaInterruptedException

--- src/main/java/uk/gov/homeoffice/digital/sas/timecard/kafka/exceptions/KafkaInterruptedException.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/timecard/kafka/exceptions/KafkaInterruptedException.java
@@ -1,8 +1,5 @@
 package uk.gov.homeoffice.digital.sas.timecard.kafka.exceptions;
 
-//@Target(ElementType.METHOD)
-//@Retention(RUNTIME)
-//@Documented
 public class KafkaInterruptedException extends InterruptedException {
   public KafkaInterruptedException(String message) {
     super(message);
